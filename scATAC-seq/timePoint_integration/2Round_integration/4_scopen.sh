1ca95839aa84b040239a478d4a15dbc8e62ad6f4
sz753404 <chengmingbo@gmail.com>
2022-06-06 15:41:03 +0200

    rename

--- scATAC-seq/timePoint_integration/2Round_integration/4_scopen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sample=$1
-
-
-scopen --input ../../2Round_Peakcalling/data/${sample}/filtered_peak_bc_matrix --input-format 10X --output-dir ./ --output-prefix Heart --max-rho 0.5 --output-format dense --verbose 1 --n-components 30
-
